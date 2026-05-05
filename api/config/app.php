8ce7c40bceab21e98688234748b87880f9c0b5ed
Edimar Cunha <cunha.eddie@gmail.com>
2026-05-05 18:55:59 -0400

    fix: adicionar frontend_url no config/app.php para redirect Google OAuth

--- api/config/app.php
+++ api/config/app.php
@@ -54,6 +54,8 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
+    'frontend_url' => env('APP_FRONTEND_URL', 'https://bovino.agr.br'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
